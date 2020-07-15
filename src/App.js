2d9887498c3881057ae7456c21b6a5d7e320795b
erammer <erammer85@gmail.com>
2020-07-15 12:54:42 -0500

    Basic Setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import NumberButton from './NumberButton.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload Part 2.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <p>
+                        Demo Calculator
+                     </p>
+                 </header>
+                <div class="ButtonGrid">
+                    <NumberButton value={1} />
+                    <NumberButton value={2} />
+                    <NumberButton value={3} />
+                    <NumberButton value={4} />
+                    <NumberButton value={5} />
+                    <NumberButton value={6} />
+                    <NumberButton value={7} />
+                    <NumberButton value={8} />
+                    <NumberButton value={9} />
+                    <NumberButton value={0} />
+                </div>
+          
+            </div>
+        );
+    }
+
+    multiply() {
+        return 8;
+    }
 }
 
 export default App;
---
+++ src/NumberButton.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+class NumberButton extends React.Component {
+    render() {
+        return (
+            <h1> {this.props.value} </h1>
+        );
+
+    }
+
+
+}
+
+export default NumberButton;
