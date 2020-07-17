93e2a6c793e8624698b578fc94c8c3d897981bde
erammer <erammer85@gmail.com>
2020-07-17 12:11:07 -0500

    Added Queries

--- src/App.js
+++ src/App.js
@@ -1,50 +1,83 @@
-import React, { Component } from 'react';
+import React, { Component, useState, useEffect } from 'react';
+import Amplify, { API, graphqlOperation } from 'aws-amplify'
+import { createTodo } from './graphql/mutations'
+import { listTodos } from './graphql/queries'
+import { onCreateTodo } from './graphql/subscriptions';
+
 import './App.css';
 import NumberButton from './NumberButton.js';
 
-class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            currentValue: 0,
-        };
+import awsExports from "./aws-exports";
+Amplify.configure(awsExports);
+
+const App = () => {
+    const [currentValue, setCurrentValue] = useState(0);
+    const [history, setHistory] = useState([]);
+
+    useEffect(() => {
+        fetchHistory()
+    },[])
+    
+    
+
+    function buttonPress(newValue) {
+
+        setCurrentValue(newValue);
+        addQuery({ name: "new", description: newValue });
     }
 
-    render() {
-        return (
-            <div className="App">
-                <header className="App-header">
-                    <p>
-                        Demo Calculator
-                     </p>
-                 </header>
-                <div class="ButtonGrid">
-                    <NumberButton value={1} onClick={() => this.setState({ currentValue: 1 })} />
-                    <NumberButton value={2} />
-                    <NumberButton value={3} />
-                    <NumberButton value={4} />
-                    <NumberButton value={5} />
-                    <NumberButton value={6} />
-                    <NumberButton value={7} />
-                    <NumberButton value={8} />
-                    <NumberButton value={9} />
-                    <NumberButton value={0} />
-                </div>
-
-                <div>{this.state.currentValue}</div>
-            </div>
-        );
+    async function addQuery(historyData) {
+        setHistory([...history, historyData])
+        await API.graphql(graphqlOperation(createTodo, { input: historyData }));
     }
 
-    buttonPress(newValue) {
-        this.setState({
-            currentValue: newValue
-        });
+    async function fetchHistory() {
+        try {
+            const todoData = await API.graphql(graphqlOperation(listTodos))
+            const history = todoData.data.listTodos.items
+            setHistory(history);
+
+          
+            
+        } catch (err) { console.log('error fetching todos') }
     }
 
-    multiply() {
+    function multiply() {
         return 8;
     }
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>
+                    Demo Calculator
+                    </p>
+            </header>
+            <div class="ButtonGrid">
+                <NumberButton value={1} onClick={() => buttonPress(1)} />
+                <NumberButton value={2} onClick={() => buttonPress(2)} />
+                <NumberButton value={3} onClick={() => buttonPress(3)} />
+                <NumberButton value={4} onClick={() => buttonPress(4)} />
+                <NumberButton value={5} onClick={() => buttonPress(5)} />
+                <NumberButton value={6} onClick={() => buttonPress(6)} />
+                <NumberButton value={7} onClick={() => buttonPress(7)} />
+                <NumberButton value={8} onClick={() => buttonPress(8)} />
+                <NumberButton value={9} onClick={() => buttonPress(9)} />
+                <NumberButton value={0} onClick={() => buttonPress(0)} />
+            </div>
+
+            <div>{currentValue}</div>
+            {
+                history.map((todo, index) => (
+                    <div key={todo.id ? todo.id : index} >
+                        <p> {todo.name} , {todo.description}</p>
+                    </div>
+                ))
+            }
+
+        </div>
+    )
+
 }
 
 export default App;
